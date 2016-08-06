3fef730c07cd9adbd1064a13c51c1474546e92ba
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2016-08-07 01:12:59 +0200

    Initial Commit

---
+++ AdvancedDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedDataStructures
+//
+//  Created by Vladislav Fitc on 07.08.16.
+//  Copyright © 2016 Fitc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
